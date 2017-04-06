13b59183b923a6f68850fcb5c33ca4436189cd5d
Fisher Adelakin <temidayoadelakin@gmail.com>
2017-04-06 03:00:52 -0400

    fallback method for getting window size

--- cactus.c
+++ cactus.c
@@ -15,7 +15,18 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 /*** data ***/
-struct termios orig_termios; // original terminal attributes
+
+// contain editor state
+struct editorConfig {
+    int screenRows;
+    int screenCols;
+    struct termios orig_termios; // original terminal attributes
+};
+
+struct editorConfig E;
+
+// implicit declaration of function 'ioctl' is invalid in C99
+int ioctl(int fd, unsigned long request, ...);
 
 /*** terminal ***/
 
@@ -29,16 +40,16 @@ void die(const char *s) {
 }
 
 void disableRawMode() {
-    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
         die("tcsetattr");
 }
 
 void enableRawMode() {
-    if(tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+    if(tcgetattr(STDIN_FILENO, &E.orig_termios) == -1)
         die("tcgetattr");
     atexit(disableRawMode); // callsdisableRawMode automatically when program exits
 
-    struct termios raw = orig_termios;
+    struct termios raw = E.orig_termios;
 
     raw.c_lflag &= ~(OPOST); // turn off all output processing
     // CS8 is a bit mask with multiple bits.
@@ -73,13 +84,53 @@ char editorReadKey() {
     return c;
 }
 
+// ioctl() isn't guaranteed to be able to request the window size on all system
+// so fallback method of getting window size
+int getCursorPosition(int *rows, int *cols) {
+    char buf[32];
+    unsigned int i = 0;
+
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+    while (i < sizeof(buf) - 1) {
+        if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+        if (buf[i] == 'R') break;
+        i++;
+    }
+    buf[i] = '\0';
+
+    if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+
+    return 0;
+}
+
+int getWindowSize(int *rows, int *cols) {
+    struct winsize ws;
+
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+        // send two escape sequences one after the other to move cursor
+        if(write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+        return getCursorPosition(rows, cols);
+    } else {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
+}
+
 /*** output ***/
 
 // handle drawing each row of the buffer of text being edited
+// drawing 24 rows for now
 void editorDrawRows() {
     int y;
-    for (y = 0; y < 24; y++) {
-        write(STDOUT_FILENO, "~\r\n", 3);
+    for (y = 0; y < E.screenRows; y++) {
+        write(STDOUT_FILENO, "~", 1);
+
+        if (y < E.screenRows -1) {
+            write(STDOUT_FILENO, "\r\n", 2);
+        }
     }
 }
 
@@ -109,8 +160,15 @@ void editorProcessKeypress() {
 }
 
 /*** init ***/
+
+// initialize all fields in the E struct
+void initEditor() {
+    if (getWindowSize(&E.screenRows, &E.screenCols) == -1) die("getWindowSize");
+}
+
 int main() {
     enableRawMode();
+    initEditor();
 
     while (1) {
         editorRefreshScreen();
