8266bea36ba3b4145fe8d965afa612c6922350f4
Fisher Adelakin <temidayoadelakin@gmail.com>
2017-04-29 05:12:48 -0400

    prevent inserting special characters

--- cactus.c
+++ cactus.c
@@ -28,6 +28,7 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 enum editorKey {
+    BACKSPACE = 127,
     ARROW_LEFT = 1000,
     ARROW_RIGHT,
     ARROW_UP,
@@ -541,6 +542,11 @@ void editorProcessKeypress() {
     int c = editorReadKey();
 
     switch(c) {
+        // ignore enter key
+        case '\r':
+            /* TODO */
+            break;
+
         case CTRL_KEY('q'):
             write(STDOUT_FILENO, "\x1b[2J", 4);
             write(STDOUT_FILENO, "\x1b[H", 3);
@@ -555,6 +561,13 @@ void editorProcessKeypress() {
             if(E.cy < E.numRows) {
                 E.cx = E.row[E.cy].size;
             }
+            break;
+
+        // handle backspace or delete key
+        case BACKSPACE:
+        case CTRL_KEY('h'):
+        case DEL_KEY:
+            /* TODO */
             break;
 
         case PAGE_UP:
@@ -578,6 +591,11 @@ void editorProcessKeypress() {
         case ARROW_LEFT:
         case ARROW_RIGHT:
             editorMoveCursor(c);
+            break;
+
+        // do nothing with the ctrl-l and escape keys
+        case CTRL_KEY('l'):
+        case '\x1b':
             break;
 
         default:
