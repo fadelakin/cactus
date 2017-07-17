2de3ae334b15920b60abaf94741e401b2249faf7
Fisher Adelakin <temidayoadelakin@gmail.com>
2017-07-16 17:02:08 -0700

    restore cursor position when cancelling search

--- cactus.c
+++ cactus.c
@@ -510,10 +510,20 @@ void editorFindCallback(char *query, int key) {
 }
 
 void editorFind() {
+    int saved_cx = E.cx;
+    int saved_cy = E.cy;
+    int saved_coloff = E.colOff;
+    int saved_rowoff = E.rowOff;
+
     char *query = editorPrompt("Search: %s (ESC to cancel)", editorFindCallback);
 
     if(query) {
         free(query);
+    } else {
+        E.cx = saved_cx;
+        E.cy = saved_cy;
+        E.colOff = saved_coloff;
+        E.rowOff = saved_rowoff;
     }
 }
 
